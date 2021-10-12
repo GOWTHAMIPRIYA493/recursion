5a87bdf8c2776b6836a664d7d45b6830df47e7a6
GOWTHAMIPRIYA493 <90253088+GOWTHAMIPRIYA493@users.noreply.github.com>
2021-10-12 15:16:45 +0530

    Create rec.c

---
+++ rec.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+  int i,factorial=1,num;
+ 
+  //printf("Enter a number: ");
+  scanf("%d",&num);
+ 
+  for(i=1;i<=num;i++)
+    factorial=factorial*i;
+ 
+  printf("%d",factorial);
+  return 0;
+}
